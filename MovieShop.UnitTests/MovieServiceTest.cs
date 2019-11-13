234802557e7657237b9cfdb19d59eaf513890705
Kevin <kramada@numurus.com>
2019-11-13 15:58:28 -0500

    added test class

---
+++ MovieShop.UnitTests/MovieServiceTest.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieShop.UnitTests
+{
+    [TestClass]
+    public class MovieServiceTest
+    {
+        [TestMethod]
+        public void TestMoviesFromFakeData1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMoviesFromFakeData2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMoviesFromFakeData3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMoviesFromFakeData4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
